3e2b05c0559a618c446599d55a8b03e2a104c32c
SvetlanaDmitrieva <sv-dmitrieva@list.ru>
2024-11-16 22:10:13 +0300

    16/11-02

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from routers import task, user
 
-
 app = FastAPI()
 
 
--- app/models/task.py
+++ app/models/task.py
@@ -14,7 +14,6 @@ class Task(Base):
     completed = Column(Boolean, default=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=True, index=True)
     slug = Column(String, unique=True, index=True)
-
     user = relationship('User', back_populates='tasks')
 
 
